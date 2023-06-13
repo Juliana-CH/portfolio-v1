4fb27d7f0d57c44cf56357e66ef6ba770fc1ecf5
Juliana <ju.hazim@gmail.com>
2023-06-13 16:38:10 +0200

    Tryning to solve translations.

--- src/components/language/Translations.jsx
+++ src/components/language/Translations.jsx
@@ -1,15 +1,22 @@
+import Menu from "../menu/Menu";
+import About from "../../views/about/About";
+
 const Translations = {
     pt: {
         "menu.home": "Início",
-        "menu.about": "Sobre mim",
+        "menu.about": "Sobre Mim",
         "menu.skills": "Habilidades",
         "menu.projects": "Projetos",
         "menu.contact": "Contato",
         "menu.closeMenu": "Fechar",
+
+        "cover.
     
         "about.text": "Sou brasileira e atualmente vivo em Valencia, Espanha. Sou casada com João, meu parceiro de vida e mãe da Maria Antônia e do Romeo, que me ensinam diariamente sobre amor. Adoro jogar beach tennis, compartilhar conhecimento e contribuir para que todos tenham oportunidades semelhantes. Também me interesso muito por alimentação saudável, consumo consciente e julgo disciplina positiva uma ferramenta útil para toda a sociedade! Tenho alguns anos de experiência em publicidade, entretanto, com o nascimento dos meus filhos, escolhi me dedicar exclusivamente à eles, mesmo sabendo que o desafio do retorno seria enorme. Chegou a hora! Estou segura e animada para equilibrar esses interesses e retomar minha carreira profissional!",
         "about.firstw": "Sobre",
-        "about.secondw": "mim"
+        "about.secondw": "mim",
+
+
         },
 
    
